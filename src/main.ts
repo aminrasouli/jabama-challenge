041736b9e586389756444e8edc2310bd2b4d9efd
aminrasouli <me@aminrasouli.ir>
2022-07-21 14:20:22 +0430

    Config And Setup OpenAPI(Swagger)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('Jabama Challenge Api')
+    .setDescription('Jabama Challenge Api Based on documentation')
+    .setExternalDoc('OpenAPI Json Schema', '/api/docs/json')
+    .setVersion('0.1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(3000);
 }
+
 bootstrap();
